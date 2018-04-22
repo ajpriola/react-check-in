170c06cba4a579fd2d4cccc105fba17ad6149e73
AJ Priola <ajpriola@gmail.com>
2018-04-22 11:55:41 -0400

    Added tests for actions and reducers

---
+++ src/__tests__/reducers/patients.test.js
@@ -0,0 +1,53 @@
+import patients from '../../reducers/patients';
+
+describe('Patients reducer', () => {
+  it('should return the initial empty state', () => {
+    expect(patients(undefined, {})).toEqual([]);
+  });
+
+  it('should handle PATIENT_LIST and store the list', () => {
+    const singlePatientList = [
+      {
+        firstName: 'First',
+        lastName: 'Tester',
+        email: 'test@email.com',
+        description: 'Test single',
+        id: 0,
+        date: new Date()
+      }
+    ];
+
+    expect(
+      patients([], {
+        type: 'PATIENT_LIST',
+        patients: singlePatientList
+      })
+    ).toEqual(singlePatientList);
+
+    const multiplePatientList = [
+      {
+        firstName: 'First',
+        lastName: 'Tester',
+        email: 'test@email.com',
+        description: 'Test single',
+        id: 0,
+        date: new Date()
+      },
+      {
+        firstName: 'Second',
+        lastName: 'Tester',
+        email: 'test@email.com',
+        description: 'Test second element',
+        id: 1,
+        date: new Date()
+      }
+    ];
+
+    expect(
+      patients([], {
+        type: 'PATIENT_LIST',
+        patients: multiplePatientList
+      })
+    ).toEqual(multiplePatientList);
+  });
+});
